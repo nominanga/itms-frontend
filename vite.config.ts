864441b24ce43e70c01530055e780f673b5a74ae
dudukin matvei <matduduk@gmail.com>
2026-03-01 17:28:56 +0300

    fully configured vite for dev and prod, defined env and vite proxy for replacing /api on dev server to remain the api requests unchanged in production, setuped dockerfile and nginx for production web app, that will be used as a container

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: "esbuild",
-    cssMinify: false
+export default defineConfig(({mode}) => {
+
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
+
+    build: {
+      outDir: 'dist',
+      sourcemap: false,
+      minify: "esbuild",
+      cssMinify: false
+    }
   }
 })
 
