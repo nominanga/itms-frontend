418f1091b64e133c220632ce9a2aebc9e3928681
dudukin matvei <matduduk@gmail.com>
2026-03-01 19:01:15 +0300

    added css reset at first, checked how does api with proxy works, all the preparation with configuration are done, proceed to coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
+import axios from "axios";
 
 function App() {
+  const [value, setValue] = useState('{}')
+
+  async function testAPIRequest() {
+    const instance = axios.create({
+      baseURL: "/api",
+      timeout: 5000
+    })
+
+    const response = await instance.get("/user/token/")
+
+    setValue(JSON.stringify(response.data))
+  }
 
   return (
     <>
+      <p>{value}</p>
+      <button onClick={testAPIRequest}>Press to update the request from the server</button>
     </>
   )
 }
